e2d265806f14680820b5ec1aa3d6aaf0c5878b3a
Anartz Mugika Ledo <mugan86@gmail.com>
2021-05-11 20:19:57 +0200

    Commit Proyecto OK

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { ApplicationRef, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { LastResultF1Component } from './last-result-f1/last-result-f1.component';
+import { Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LastResultF1Component
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, LastResultF1Component]
+})
+export class AppModule {
+  // Para convertir en web component
+  constructor(private injector: Injector) {
+    const elementCustom = createCustomElement(
+      LastResultF1Component,
+      {
+        injector: this.injector
+      }
+    );
+    customElements.define('last-f1-results-widget', elementCustom);
+  }
+  ngDoBootstrap(appRef: ApplicationRef): void {
+    if (document.querySelector('app-root')) {
+      appRef.bootstrap(AppComponent);
+    }
+  }
+}
